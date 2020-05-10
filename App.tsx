7478dea020bda1265407937bf46b05d10598560f
James Chetwood <james@jameschetwood.co.uk>
2020-05-10 10:40:24 +0100

    Init

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import * as Speech from "expo-speech";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  setInterval(()=>{
+    Speech.speak(`I am a test`);
+  }, 2000);
+
   return (
-    <View style={styles.container}>
+     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
